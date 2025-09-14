383f8d41f4bbf08a87430edba57dae2805582b70
idkwhattonamemyself115 <khu27@nmhschool.org>
2025-09-14 15:51:53 -0400

    main file & menu fixed (with user input)

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import json
 with open("restaurant.json", "r") as f:
     rest_data = json.load(f)
 print(rest_data)
+
+userinput=input("What would you like to do? view_menu_items, search_item, add_new_item, upload_existing_data, delete_item ")
 import menu_item
-print(rest_data['categories'])
-menu_item.menu_item.view_menu_items(menu_item.menu_item.data)
+if userinput == 'view_menu_items':
+    menu_item.add_new_item(menu_item.data)
+elif userinput == 'search_item':
+    menu_item.search_item(menu_item.data)
+elif userinput == 'add_new_item':
+    menu_item.add_new_item(menu_item.data)
+elif userinput == 'upload_existing_data':
+    menu_item.upload_existing_data(menu_item.data)
+elif userinput == 'delete_item':
+    menu_item.delete_item(menu_item.data)
+else:
+    print("Invalid input")
+    userinput=input("What would you like to do? view_menu_items, search_item, add_new_item, upload_existing_data, delete_item ")
